89abc92b57d0d6b833d14ee08e9d817a7c970712
KarunaVaddi <vaddi.karuna12@gmail.com>
2024-10-06 18:50:38 +0530

    first commit

---
+++ src/pages/showUser.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function NewPost() {
+  return (
+    <div>
+      <Navbar />
+      <h2>show user</h2>
+    </div>
+  )
+}
